3a913be8ddc49f3be1d8b4c2c152aedb5f2e28af
islam-heddi <117187090+islam-heddi@users.noreply.github.com>
2023-07-06 11:36:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mark_all(){
+    document.getElementById("newer").style.display = "none";
+    document.getElementById("newer2").style.display = "none";
+    document.getElementById("newer3").style.display = "none";
+
+    document.getElementById("new-notification").innerHTML = 0;
+}
